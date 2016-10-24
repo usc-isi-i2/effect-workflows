526c52bc707beab84fd301e8685afc3334308d99
darkshadows123 <rjain034@gmail.com>
2016-10-23 20:34:52 -0700

    changes in api file

--- scripts/APIDownloader/darknetAPIToJl.py
+++ scripts/APIDownloader/darknetAPIToJl.py
@@ -15,26 +15,38 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print ("Got arguments:", args)
 
-    headers = {"userId" :"usc","apiKey": args.apiKey}
+    headers = {"userId" :"usc","apiKey": args.apiKey, "Connection" : "close"}
 
     def write_output_to_file(file_name, result):
         out_file = open(file_name, 'w')
         for line in result:
-            line = json.dumps(line)
+            line = json.dumps(line, ensure_ascii=False)
             out_file.write(line + "\n")
 
     def get_all_urls():
-        zeroDayUrl = "https://54.186.69.219:443/GargoyleApi/getZerodayProducts?limit=10000&from=" + args.fromDate
-        hackingItemsUrl = "https:// https://54.186.69.219:443/GargoyleApi/getHackingItems?limit=10000&from=" + args.fromDate
-        dictionaryUrl = "https:// https://54.186.69.219:443/GargoyleApi/getDictionary?limit=10000&from=" + args.fromDate
-        clusterStatisticsUrl = "https:// https://54.186.69.219:443/GargoyleApi/getClusterStatistics?limit=10000&from" + args.fromDate
-        hackingPostsUrl = "https:// https://54.186.69.219:443/GargoyleApi/getHackingPosts?limit=10000&from=" + args.fromDate
-        getTableUrl = "https://54.186.69.219:443/GargoyleApi/getTable?limit=10000&tableName="
-        return [zeroDayUrl,hackingItemsUrl,dictionaryUrl,clusterStatisticsUrl,hackingPostsUrl,getTableUrl]
+        zeroDayUrl = "https://54.186.69.219/GargoyleApi/getZerodayProducts?limit=10&from=" + args.fromDate
+        hackingItemsUrl = "https://54.186.69.219/GargoyleApi/getHackingItems?limit=10&from=" + args.fromDate
+        dictionaryUrl = "https://54.186.69.219/GargoyleApi/getDictionary?limit=10&from=" + args.fromDate
+        clusterStatisticsUrl = "https://54.186.69.219/GargoyleApi/getClusterStatistics?limit=10&from" + args.fromDate
+        hackingPostsUrl = "https://54.186.69.219/GargoyleApi/getHackingPosts?limit=10&from=" + args.fromDate
+        return [zeroDayUrl,hackingItemsUrl,dictionaryUrl,clusterStatisticsUrl,hackingPostsUrl]
+
+
+    def byteify(input):
+        if isinstance(input, dict):
+            return {byteify(key): byteify(value)
+                    for key, value in input.iteritems()}
+        elif isinstance(input, list):
+            return [byteify(element) for element in input]
+        elif isinstance(input, unicode):
+            return input.encode('utf-8')
+        else:
+            return input
 
     def get_result(url):
         response = requests.get(url, verify=False,  headers=headers)
-        return json.loads(response.text)
+        print type(response.text)
+        return byteify(json.loads(response.text))
 
     urls = get_all_urls()
     for url in urls:
@@ -42,32 +54,21 @@ if __name__ == "__main__":
         if (re.search(api_name, url, re.IGNORECASE)):
             res = get_result(url)
             write_output_to_file(api_name + ".jl", res['results'])
-
         api_name = "hackingitems"
         if (re.search(api_name, url, re.IGNORECASE)):
             res = get_result(url)
             write_output_to_file(api_name + ".jl", res['results'])
-
         api_name = "dictionary"
         if (re.search(api_name, url, re.IGNORECASE)):
             res = get_result(url)
             write_output_to_file(api_name + ".jl", res['results'])
-
         api_name = "clusterstatistics"
         if (re.search(api_name, url, re.IGNORECASE)):
             res = get_result(url)
             write_output_to_file(api_name + ".jl", res['results'])
-
         api_name = "hackingposts"
         if (re.search(api_name, url, re.IGNORECASE)):
             res = get_result(url)
             out_file = open(api_name + ".jl", 'w')
             for each_number in res['results'].keys():
-                out_file.write(json.dumps(res['results'][each_number]))
-
-        api_name = "table"
-        if (re.search(api_name, url, re.IGNORECASE)):
-            tableNames = ["Classification","Cluster","CurrencyMaster","Forums","ItemClassificationMap","ItemClusterMapping","Items","ItemSellingPriceUSD","LabelsTraining","Language","Marketplaces","Posts","SellingPrice","DictionaryTranslation","TopicClassificationMap","Users","Vendors"]
-            for table in tableNames:
-                res = get_result(url + table)
-                write_output_to_file(api_name + "_" + table, res['data'])
+                out_file.write(json.dumps(res['results'][each_number],ensure_ascii=False))
