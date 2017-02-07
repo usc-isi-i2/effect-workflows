79ee3b6b075a3dfca629245d7425a01986c50821
darkshadows123 <rjain034@gmail.com>
2017-02-07 15:00:31 -0800

    add hg cpe api without date

---
+++ scripts/APIDownloader/hgCPEAPI.py
@@ -0,0 +1,40 @@
+from APIDownloader import APIDownloader
+from argparse import ArgumentParser
+from pyspark import SparkContext, StorageLevel
+from pyspark.sql import HiveContext
+import json
+from dateUtil import DateUtil
+
+'''
+spark-submit --deploy-mode client \
+    --py-files /home/hadoop/effect-workflows/lib/python-lib.zip \
+    hgCPEAPI.py \
+    --outputFolder <HDFS or s3 output folder> \
+    --team "hyperiongray" \
+    --password <PASSWORD> \
+'''
+
+if __name__ == "__main__":
+
+    sc = SparkContext()
+    sqlContext = HiveContext(sc)
+
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--outputFolder", type=str, help="Output foldername", required=True)
+    parser.add_argument("-t", "--team", type=str, help="Team Name", required=True)
+    parser.add_argument("-p", "--password", type=str, help="password for connecting to hyperion gray api", required=True)
+
+    args = parser.parse_args()
+    print ("Got arguments:", args)
+
+    url_cpe = "https://effect.hyperiongray.com/api/cpe/"
+
+    apiDownloader = APIDownloader(sc, sqlContext)
+
+    results = apiDownloader.download_api(url_cpe, "isi", args.password)
+    if results is not None:
+        print "Downloaded ", len(results), " new CPE data rows. Adding them to CDR"
+        if len(results) > 0:
+            rdd = sc.parallelize(results)
+            rdd.map(lambda x: ("hg-cpe", json.dumps(x))).saveAsSequenceFile(args.outputFolder + "/hg-cpe")
+            apiDownloader.load_into_cdr(results, "hg_cpe", args.team, "hg-cpe")
