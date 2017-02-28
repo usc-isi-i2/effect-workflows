5bfab2adb76a7e81d2d26d3685f2f42e60307e27
dkapoor <dipsykapoor@gmail.com>
2017-02-28 20:58:07 +0000

    Update the script to make extractor to include digWorkflow

--- make-extractor.sh
+++ make-extractor.sh
@@ -1,4 +1,10 @@
+WORKFLOWSBASE=/mnt/github/dig-workflows
+
+CONDA_PY_DEST=effect-env/lib/python2.6/site-packages
 conda env create -f environment.yml
 rm -rf effect-env
 conda create -m -p $(pwd)/effect-env/ --copy --clone effect-env
+cp cdrLoader.py $CONDA_PY_DEST/
+mkdir $CONDA_PY_DEST/digWorkflow
+cp $WORKFLOWSBASE/pySpark-workflows/digWorkflow/* $CONDA_PY_DEST/digWorkflow/ 
 zip -r effect-env.zip effect-env
