28b9cab9d2cce39d55a5d5d5ed70e9b8ff5e7e8c
Dipsy Kapoor <dipsykapoor@gmail.com>
2017-03-16 11:07:54 -0700

    update workflow to be able to frame and run karma separately, check for folders before framing

--- effectWorkflow.py
+++ effectWorkflow.py
@@ -12,6 +12,7 @@ from argparse import ArgumentParser
 from digRegexExtractor.regex_extractor import RegexExtractor
 from digExtractor.extractor_processor import ExtractorProcessor
 import re
+from hdfs import Config
 
 '''
 spark-submit --deploy-mode client  \
@@ -65,7 +66,6 @@ class EffectWorkflow(Workflow):
             return self.reduce_rdds_with_settings({"karma.provenance.properties":"source,publisher,dateRecorded:date"}, outpartitions, *result_rdds)
         return None
 
-
 if __name__ == "__main__":
     sc = SparkContext()
     sqlContext = HiveContext(sc)
@@ -73,6 +73,7 @@ if __name__ == "__main__":
     java_import(sc._jvm, "edu.isi.karma")
     workflow = EffectWorkflow(sc, sqlContext)
     fileUtil = FileUtil(sc)
+    hdfs_client = Config().get_client()
 
     parser = ArgumentParser()
     parser.add_argument("-i", "--inputTable", help="Input Table", required=True)
@@ -80,7 +81,9 @@ if __name__ == "__main__":
     parser.add_argument("-n", "--partitions", help="Number of partitions", required=False, default=20)
     parser.add_argument("-t", "--outputtype", help="Output file type - text or sequence", required=False, default="sequence")
     parser.add_argument("-q", "--query", help="HIVE query to get data", default="", required=False)
-    parser.add_argument("-f", "--frame", help="Start with framer", type=bool, default=False, required=False)
+    parser.add_argument("-k", "--karma", help="Run Karma", default=False, required=False, action="store_true")
+    parser.add_argument("-f", "--framer", help="Run the framer", default=False, required=False, action="store_true")
+
     args = parser.parse_args()
     print ("Got arguments:", args)
 
@@ -92,8 +95,8 @@ if __name__ == "__main__":
 
     numFramerPartitions = numPartitions/2
 
-    if args.frame:
-        reduced_rdd = sc.sequenceFile(outputFilename + "/reduced_rdd").mapValues(lambda x: json.loads(x))
+    if not args.karma:
+        reduced_rdd = sc.sequenceFile(outputFilename + "/reduced_rdd").mapValues(lambda x: json.loads(x)).persist(StorageLevel.MEMORY_AND_DISK)
     else:
         if len(hiveQuery) > 0:
             cdr_data = workflow.load_cdr_from_hive_query(hiveQuery)\
@@ -130,71 +133,85 @@ if __name__ == "__main__":
         # reduced_rdd = None
         # Run karma model as per the source of the data
         reduced_rdd = workflow.apply_karma_model_per_msg_type(cdr_extractions_rdd, models, context_url, base_uri,
-                                                                  numPartitions, numFramerPartitions)
+                                                                  numPartitions, numFramerPartitions).persist(StorageLevel.MEMORY_AND_DISK)
+        fileUtil.save_file(reduced_rdd, outputFilename + '/reduced_rdd', outputFileType, "json")
 
     # Frame the results
     if reduced_rdd is not None:
-        reduced_rdd = reduced_rdd.persist(StorageLevel.MEMORY_AND_DISK)
-        # print reduced_rdd.first()
-        if args.frame == False:
-            fileUtil.save_file(reduced_rdd, outputFilename + '/reduced_rdd', outputFileType, "json")
-        reduced_rdd.setName("karma_out_reduced")
+        if args.framer:
+            reduced_rdd.setName("karma_out_reduced")
 
-        types = [
-            {"name": "AttackEvent", "uri": "http://schema.dig.isi.edu/ontology/AttackEvent"},
-            {"name": "EmailAddress", "uri": "http://schema.dig.isi.edu/ontology/EmailAddress"},
-            {"name": "GeoCoordinates", "uri": "http://schema.org/GeoCoordinates"},
-            {"name": "Organization", "uri": "http://schema.org/Organization"},
-            {"name": "PersonOrOrganization", "uri": "http://schema.dig.isi.edu/ontology/PersonOrOrganization"},
-            {"name": "PhoneNumber", "uri": "http://schema.dig.isi.edu/ontology/PhoneNumber"},
-            {"name": "Place", "uri": "http://schema.org/Place"},
-            {"name": "PostalAddress", "uri": "http://schema.org/PostalAddress"},
-            {"name": "Vulnerability", "uri": "http://schema.dig.isi.edu/ontology/Vulnerability"},
-            {"name": "SoftwareSystem", "uri":"http://schema.dig.isi.edu/ontology/SoftwareSystem"},
-            {"name": "Identifier", "uri":"http://schema.dig.isi.edu/ontology/Identifier"},
-            {"name": "CVSS", "uri":"http://schema.dig.isi.edu/ontology/CVSS"},
-            {"name": "PriceSpecification", "uri": "http://schema.dig.isi.edu/ontology/PriceSpecification"},
-            {"name": "Exploit", "uri": "http://schema.dig.isi.edu/ontology/Exploit"},
-            {"name": "LoginCredentials", "uri": "http://schema.dig.isi.edu/ontology/LoginCredentials"},
-            {"name": "UserName", "uri": "http://schema.dig.isi.edu/ontology/UserName"},
-            {"name": "Password", "uri": "http://schema.dig.isi.edu/ontology/Password"},
-            {"name": "Topic", "uri":"http://schema.dig.isi.edu/ontology/Topic"},
-            {"name": "Post", "uri": "http://schema.dig.isi.edu/ontology/Post"},
-            {"name": "Forum", "uri": "http://schema.dig.isi.edu/ontology/Forum"},
-            {"name": "Malware", "uri": "http://schema.dig.isi.edu/ontology/Forum"},
-            {"name": "IPAddress", "uri": "http://schema.dig.isi.edu/ontology/IPAddress"},
-            {"name": "ComputerHardware", "uri": "http://schema.dig.isi.edu/ontology/ComputerHardware"},
-            {"name": "Blog", "uri": "http://schema.org/Blog"}
-        ]
+            types = [
+                {"name": "AttackEvent", "uri": "http://schema.dig.isi.edu/ontology/AttackEvent"},
+                {"name": "EmailAddress", "uri": "http://schema.dig.isi.edu/ontology/EmailAddress"},
+                {"name": "GeoCoordinates", "uri": "http://schema.org/GeoCoordinates"},
+                {"name": "Organization", "uri": "http://schema.org/Organization"},
+                {"name": "PersonOrOrganization", "uri": "http://schema.dig.isi.edu/ontology/PersonOrOrganization"},
+                {"name": "PhoneNumber", "uri": "http://schema.dig.isi.edu/ontology/PhoneNumber"},
+                {"name": "Place", "uri": "http://schema.org/Place"},
+                {"name": "PostalAddress", "uri": "http://schema.org/PostalAddress"},
+                {"name": "Vulnerability", "uri": "http://schema.dig.isi.edu/ontology/Vulnerability"},
+                {"name": "SoftwareSystem", "uri":"http://schema.dig.isi.edu/ontology/SoftwareSystem"},
+                {"name": "Identifier", "uri":"http://schema.dig.isi.edu/ontology/Identifier"},
+                {"name": "CVSS", "uri":"http://schema.dig.isi.edu/ontology/CVSS"},
+                {"name": "PriceSpecification", "uri": "http://schema.dig.isi.edu/ontology/PriceSpecification"},
+                {"name": "Exploit", "uri": "http://schema.dig.isi.edu/ontology/Exploit"},
+                {"name": "LoginCredentials", "uri": "http://schema.dig.isi.edu/ontology/LoginCredentials"},
+                {"name": "UserName", "uri": "http://schema.dig.isi.edu/ontology/UserName"},
+                {"name": "Password", "uri": "http://schema.dig.isi.edu/ontology/Password"},
+                {"name": "Topic", "uri":"http://schema.dig.isi.edu/ontology/Topic"},
+                {"name": "Post", "uri": "http://schema.dig.isi.edu/ontology/Post"},
+                {"name": "Forum", "uri": "http://schema.dig.isi.edu/ontology/Forum"},
+                {"name": "Malware", "uri": "http://schema.dig.isi.edu/ontology/Forum"},
+                {"name": "IPAddress", "uri": "http://schema.dig.isi.edu/ontology/IPAddress"},
+                {"name": "ComputerHardware", "uri": "http://schema.dig.isi.edu/ontology/ComputerHardware"},
+                {"name": "Blog", "uri": "http://schema.org/Blog"}
+            ]
 
-        frames = [
-            {"name": "attack", "url": "https://raw.githubusercontent.com/usc-isi-i2/effect-alignment/master/frames/attackevent.json"},
-            {"name": "vulnerability", "url": "https://raw.githubusercontent.com/usc-isi-i2/effect-alignment/master/frames/vulnerability.json"},
-            {"name": "exploit", "url": "https://raw.githubusercontent.com/usc-isi-i2/effect-alignment/master/frames/exploit.json"},
-            {"name": "topic", "url": "https://raw.githubusercontent.com/usc-isi-i2/effect-alignment/master/frames/topic.json"},
-            {"name": "post", "url": "https://raw.githubusercontent.com/usc-isi-i2/effect-alignment/master/frames/post.json"},
-            {"name": "malware", "url": "https://raw.githubusercontent.com/usc-isi-i2/effect-alignment/master/frames/malware.json"},
-            {"name": "blog", "url": "https://raw.githubusercontent.com/usc-isi-i2/effect-alignment/master/frames/blog.json"}
-        ]
-        type_to_rdd_json = workflow.apply_partition_on_types(reduced_rdd, types)
+            all_frames = [
+                {"name": "attack", "url": "https://raw.githubusercontent.com/usc-isi-i2/effect-alignment/master/frames/attackevent.json"},
+                {"name": "vulnerability", "url": "https://raw.githubusercontent.com/usc-isi-i2/effect-alignment/master/frames/vulnerability.json"},
+                {"name": "exploit", "url": "https://raw.githubusercontent.com/usc-isi-i2/effect-alignment/master/frames/exploit.json"},
+                {"name": "topic", "url": "https://raw.githubusercontent.com/usc-isi-i2/effect-alignment/master/frames/topic.json"},
+                {"name": "post", "url": "https://raw.githubusercontent.com/usc-isi-i2/effect-alignment/master/frames/post.json"},
+                {"name": "malware", "url": "https://raw.githubusercontent.com/usc-isi-i2/effect-alignment/master/frames/malware.json"},
+                {"name": "blog", "url": "https://raw.githubusercontent.com/usc-isi-i2/effect-alignment/master/frames/blog.json"}
+            ]
+            frames = []
+            # If there is a restart and the frames are already done, dont restart them
+            for frame in all_frames:
+                name = frame.name
+                include_frame = True
+                if hdfs_client.status(outputFilename + "/" + name, False):
+                    #Folder exists, check if it was created successfully
+                    if hdfs_client.status(outputFilename + "/" + name + "/_SUCCESS", False):
+                        #There is success folder, so its already frames
+                        include_frame = False
+                    else:
+                        #No success file, but folder exists, so delete the folder
+                        hdfs_client.delete(outputFilename + "/" + name)
+                if include_frame is True:
+                    frames.append(frame)
 
-        for type_name in type_to_rdd_json:
-            type_to_rdd_json[type_name]["rdd"] = type_to_rdd_json[type_name]["rdd"].persist(StorageLevel.MEMORY_AND_DISK)
-            type_to_rdd_json[type_name]["rdd"].setName(type_name)
+            type_to_rdd_json = workflow.apply_partition_on_types(reduced_rdd, types)
 
-        framer_output = workflow.apply_framer(reduced_rdd, type_to_rdd_json, frames,
-                                              numFramerPartitions,
-                                              None)
+            for type_name in type_to_rdd_json:
+                type_to_rdd_json[type_name]["rdd"] = type_to_rdd_json[type_name]["rdd"].persist(StorageLevel.MEMORY_AND_DISK)
+                type_to_rdd_json[type_name]["rdd"].setName(type_name)
 
-        for frame_name in framer_output:
-            framer_output_one_frame = framer_output[frame_name] #.coalesce(numFramerPartitions)
-            print "Save frame:", frame_name
-            if not framer_output_one_frame.isEmpty():
-                fileUtil.save_file(framer_output_one_frame, outputFilename + '/' + frame_name, outputFileType, "json")
+            framer_output = workflow.apply_framer(reduced_rdd, type_to_rdd_json, frames,
+                                                  numFramerPartitions,
+                                                  None)
 
-        reduced_rdd.unpersist()
-        for type_name in type_to_rdd_json:
-            type_to_rdd_json[type_name]["rdd"].unpersist()
+            for frame_name in framer_output:
+                framer_output_one_frame = framer_output[frame_name] #.coalesce(numFramerPartitions)
+                print "Save frame:", frame_name
+                if not framer_output_one_frame.isEmpty():
+                    fileUtil.save_file(framer_output_one_frame, outputFilename + '/' + frame_name, outputFileType, "json")
 
-    if cdr_extractions_rdd:
+            reduced_rdd.unpersist()
+            for type_name in type_to_rdd_json:
+                type_to_rdd_json[type_name]["rdd"].unpersist()
+
+    if args.karma:
         cdr_extractions_rdd.unpersist()
