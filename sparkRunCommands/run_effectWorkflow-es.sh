3fb7653ed01edc2e4b297d2f73cabe54a23f1820
Dipsy Kapoor <dipsykapoor@gmail.com>
2017-10-27 12:21:27 -0700

    updated ES workflow settings

--- sparkRunCommands/run_effectWorkflow-es.sh
+++ sparkRunCommands/run_effectWorkflow-es.sh
@@ -1,10 +1,10 @@
 spark-submit --master yarn --deploy-mode client  \
     --driver-memory 10g \
-    --executor-memory 10g --num-executors 25 --executor-cores 5 \
+  --executor-memory 24g --num-executors 30 --executor-cores 2 \
     --jars "elasticsearch-hadoop-2.4.0.jar" \
     --py-files python-lib.zip \
     effectWorkflow-es.py \
-    --host 128.9.35.71 \
+    --host "128.9.35.71, 128.9.35.72, 128.9.35.73, 128.9.35.74" \
     --port 9200 \
     --hdfsManager "http://cloudmgr03.isi.edu:50070" \
     $@
