657033c5e99b73613e1d3d42cf3e87216e58b511
greatyyx <bigyyx@gmail.com>
2018-11-19 15:06:42 -0800

    update spark command line

--- scripts/APIDownloader/hgUmichPhishAlerts.py
+++ scripts/APIDownloader/hgUmichPhishAlerts.py
@@ -8,7 +8,7 @@ from dateUtil import DateUtil
 '''
 spark-submit --deploy-mode client \
     --py-files /home/hadoop/effect-workflows/lib/python-lib.zip \
-    hgCPEAPI.py \
+    hgUmichPhishAlerts.py \
     --outputFolder <HDFS or s3 output folder> \
     --team "hyperiongray" \
     --password <PASSWORD> \
