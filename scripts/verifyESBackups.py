5f16d05cf53fc1c661a256c167083d3a23c5f2c9
greatyyx <bigyyx@gmail.com>
2018-11-26 10:47:29 -0800

    update regex to ignore backup files

--- scripts/verifyESBackups.py
+++ scripts/verifyESBackups.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     from_addr = 'oozie@isi.edu'
     to_addr = ['yixiangy@isi.edu']
 
-    files = [f for f in os.listdir("/data/lockheed/upload") if re.match(r'data-2[0-9]*.json.gz', f)]
+    files = [f for f in os.listdir("/data/lockheed/upload") if re.match(r'^data-2[0-9]*.json.gz$', f)]
     files.sort()
     num_files = len(files)
     size = 0
